9a82d5a2fff79a14e126ad5398402e3e320596ff
dimas pradipto <dpradipto8@gmail.com>
2026-08-03 07:42:46 +0700

    feat: implement dashboard layout and basic authentication controllers

---
+++ resources/views/layouts/dashboard/footer.blade.php
@@ -0,0 +1,6 @@
+  <!-- ======= Footer ======= -->
+  <footer id="footer" class="footer">
+      <div class="copyright">
+          &copy; Copyright <strong><span>SIPENA - UNIVERSITAS IBNU SINA</span></strong>. All Rights Reserved
+      </div>
+  </footer><!-- End Footer -->
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+
+
+Route::controller(AuthController::class)->group(function () {
+    Route::get('/', 'login')->name('login');
+});
+
+Route::middleware(['auth', 'checkrole'])->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard.index');
 });
